4873e32433551101e06eb959ea69bd85352e59bc
Jordon Smith <jordon@teamwork.com>
2015-07-04 14:24:32 +0100

    O_O

---
+++ personal/smooth-scrolling.el
@@ -0,0 +1,6 @@
+(prelude-require-package 'smooth-scrolling)
+(require 'smooth-scrolling)
+
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
